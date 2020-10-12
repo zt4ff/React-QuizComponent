76812c9939b115ae77215f20df4156d8c7512f29
zt4ff <segunkayode00@gmail.com>
2020-10-12 16:40:44 +0100

    fix module 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,9 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <div className="QuizQuestion">{quizData[0].instruction_text}</div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
       </div>
     );
   }
