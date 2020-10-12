7e7d03455396fc7088e2cef6da86f852f88c2594
zt4ff <segunkayode00@gmail.com>
2020-10-12 16:56:54 +0100

    module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
 
 let quizData = require("./quiz_data.json");
 
@@ -11,9 +12,7 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
+        <QuizQuestion quiz_question={this.state.quiz_position - 1} />
       </div>
     );
   }
---
+++ src/QuizQuestion.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <p>// instruction text goes here</p>
+        </section>
+        <section className="buttons">
+          <ul>//quiz question button logic goes here</ul>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default QuizQuestion;
